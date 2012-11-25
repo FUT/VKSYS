72acbdb2925d7c63765ecbb8b2df7ac2a983b0de
Yury Kaliada <fut.wrk@gmail.com>
2012-11-25 16:01:28 +0300

    Encoded packer printer byte partition fixed
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- network_packets/printer.rb
+++ network_packets/printer.rb
@@ -1,6 +1,6 @@
 class Printer
   def self.print_by_sections(flow, message)
-    message.scan(/.{8}/).each do |byte|
+    message.scan(/.{,8}/).each do |byte|
       flow.para byte, font: 'courier'
     end
   end

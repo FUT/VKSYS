da70ba4c48612247ba0e6ef43afa358e99d85995
Yury Kaliada <fut.wrk@gmail.com>
2013-02-02 11:15:55 +0300

    Groups for multicast messages
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- broadcast/client.rb
+++ broadcast/client.rb
@@ -4,23 +4,26 @@ require 'shoes'
 
 
 MODES = %w{broadcast unicast multicast anycast}
+GROUPS = %w{home work university friends}
 PORT = rand(1000) + 50000
 
-$channel = Cod.tcp("localhost:44446")
+$channel = Cod.tcp("localhost:44440")
 $channel.interact ['connect', [], PORT]
 
 Shoes.app title: PORT.to_s, width: 400 do
   para 'Message'
   @message_line = edit_line width: 400
-  para 'Ports'
+  para 'Port'
   @ports = edit_line width: 400
   @messages = flow width: 400, height: 300, scroll: true
+  @type_select = list_box items: MODES, width: 150, height: 40, choose: 'unicast'
+  @group_select = list_box items: GROUPS, width: 150, height: 40 do |list|
+    $channel.interact ['change_group', '', @group_select.text, PORT]
+  end
 
-  MODES.each do |mode|
-    button mode do
-      $channel.interact [mode, @ports.text.split(' '), @message_line.text, PORT]
-      @message_line.text = ''
-    end
+  button 'Send', height: 40 do
+    $channel.interact ["#{@type_select.text} #{@group_select.text}", @ports.text.split(' '), @message_line.text, PORT]
+    @message_line.text = ''
   end
 
   Thread.new do
@@ -32,7 +35,6 @@ Shoes.app title: PORT.to_s, width: 400 do
       when 'append'
         @messages.para message
         server_channel.put Time.now
-
       else
         server_channel.put 'Unknown request!'
       end
--- broadcast/server.rb
+++ broadcast/server.rb
@@ -1,11 +1,16 @@
 require 'cod'
 require 'pry'
 
-time_server_channel = Cod.tcp_server('localhost:44446')
+time_server_channel = Cod.tcp_server('localhost:44440')
 @clients = []
+@groups = Hash.new []
 
 def dest(channel)
   channel.instance_eval('@destination').split(':').last
+end
+
+def get_client_by_port(port)
+  @clients.find { |client| dest(client) == port }
 end
 
 def respond(message, ports = [])
@@ -25,19 +30,23 @@ loop do
   (mode, ports, message, client_port), client_channel = time_server_channel.get_ext
   puts "[FROM #{client_port}] Mode #{mode} Message #{message}"
 
-  case mode
+  case mode.split.first
+  when 'change_group'
+    @groups.each_pair { |key, value| @groups[key] = value - [client_port] }
+    @groups[message] += [client_port]
   when 'connect'
     @clients << Cod.tcp("localhost:#{message}")
     puts "Client on port #{message} connected"
-
   when 'unicast'
     respond(message) { |port| port == ports.first }
   when 'multicast'
-    respond(message) { |port| ports.include? port }
+    puts @groups, client_port, message
+    respond(message) { |port| @groups[mode.split[1]].include? port.to_i }
   when 'broadcast'
     respond(message)
   when 'anycast'
-    respond(message) { |port| close_ports(client_port).include? port }
+    pp = close_ports(client_port)[rand(close_ports(client_port).length)]
+    respond(message) { |port| pp == port }
   else
     puts 'ERROR! UNKNOWN REQUEST!'
   end
